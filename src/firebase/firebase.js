2d0429cb45430ed2ca5c7841eec8ded33a544bb3
Matthew Killer <mykillergamer@gmail.com>
2018-06-11 08:43:50 +0200

    third firebase fix attempt

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase';
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIRBASE_AUTH_DOMAIN,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -11,9 +11,11 @@ const config = {
 
 firebase.initializeApp(config);
 
-// const database = firebase.database();
+const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider;
-// console.log("NODE_ENV",process.env.NODE_ENV);
-// console.log("API_KEY",process.env.APP_KEY);
+console.log("NODE_ENV",process.env.NODE_ENV);
+console.log("API_KEY",process.env.APP_KEY);
 
 export { firebase, googleAuthProvider};
+
+/* Don't forget to use heroku config:set for the dev config variables */
